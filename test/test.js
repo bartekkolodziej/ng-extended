ee932567d4de88c2fe53e9e891e1081595a0c9b2
kolkol69 <maksym.kolodiy@netguru.pl>
2019-04-22 23:06:15 +0200

    Add test for nge-add

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+describe('nge-add', function() {
+    describe('sidebar', function() {
+        it('should add content to given component.ts, component.html, and module.ts', function() {
+            exec("nge-add ./angular-app/src/app/app ./angular-app/src/app/app sidebar --position='right'", function callback(error, stdout, stderr){
+                let moduleData = fs.readFileSync('./angular-app/src/app/app' + '.module.ts'); //read existing contents into data
+                let tsData = fs.readFileSync('./angular-app/src/app/app' + '.component.ts'); //read existing contents into data
+                let htmlData = fs.readFileSync('./angular-app/src/app/app' + '.component.html'); //read existing contents into data
+
+                assert.equal(moduleData.toString().includes('SidebarModule'), true);
+                assert.equal(tsData.toString().includes('displaySidebar'), true);
+                assert.equal(htmlData.toString().includes('</p-sidebar>'), true);
+            });
+        });
+    });
+});
