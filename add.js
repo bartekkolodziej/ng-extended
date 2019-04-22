f7bb28cdbdd6fada1a35e832e8e7251efa9d58e3
kolkol69 <maksym.kolodiy@netguru.pl>
2019-04-22 22:07:55 +0200

    Add fieldset component

--- add.js
+++ add.js
@@ -35,6 +35,24 @@ switch (elementToAdd) {
     case 'tabview':
         addTabview();
         break;
+    case 'fieldset':
+        addFieldSet();
+        break;
+}
+
+function addFieldSet() {
+    let htmlToAppend = `<p-fieldset legend="Toggleable" [toggleable]="true">
+    The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
+    His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
+    Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
+    kind and benevolent to those who give respect, 
+    but given to ruthless violence whenever anything stands against the good of the family.
+</p-fieldset>`;
+    let tsToAppend = ``;
+
+    updateHtmlFile(htmlToAppend);
+    updateTsFile(tsToAppend);
+    updateModule("import {FieldsetModule} from 'primeng/fieldset';\n", '\t\t FieldsetModule');
 }
 
 function addTabview() {
