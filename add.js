71d4b4fd6291a238df33f2ce2378342016c9ddf0
kolkol69 <maksym.kolodiy@netguru.pl>
2019-04-23 20:21:33 +0200

    Add support for argumentsAsHtml

--- add.js
+++ add.js
@@ -41,7 +41,7 @@ switch (elementToAdd) {
 }
 
 function addFieldSet() {
-    let htmlToAppend = `<p-fieldset legend="Toggleable" [toggleable]="true">
+    let htmlToAppend = `<p-fieldset legend="Toggleable" [toggleable]="true" ${argumentsAsHtml}>
     The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding. 
     His beloved son Michael has just come home from the war, but does not intend to become part of his father's business. 
     Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, 
@@ -57,7 +57,7 @@ function addFieldSet() {
 
 function addTabview() {
     let htmlToAppend = `<h3>Closable</h3>
-    <p-tabView>
+    <p-tabView ${argumentsAsHtml}>
         <p-tabPanel header="Godfather I" [selected]="true">
             The story begins as Don Vito Corleone, the head of a New York Mafia family, overseeshis daughter's wedding. His beloved son ichael has just come home from the war, but does not intend to become part of his father's business. T hrough Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family, kind and benevolent to those who give respect, but given to ruthless violence whenever anything stands against the good of the family.
         </p-tabPanel>
